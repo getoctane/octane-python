f660482c1b8689c0e6acf78a247a2482992ad74f
piyushoct <110411253+piyushoct@users.noreply.github.com>
2023-02-23 11:48:09 -0500

    feat: Add bulk upload for measurements (#7)

--- octane/api_resources/measurement.py
+++ octane/api_resources/measurement.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import, division, print_function
+
 from datetime import datetime
 
 from octane.api_resources.abstract import CreateableAPIResource
+from octane import api_requestor, util
 
 
 class Measurement(
@@ -16,4 +18,31 @@ class Measurement(
         if "time" not in params:
             params["time"] = datetime.utcnow().isoformat()
 
-        return super(Measurement, cls).create(api_key=api_key, idempotency_key=idempotency_key, octane_version=octane_version, **params)
+        return super(Measurement, cls).create(
+            api_key=api_key,
+            idempotency_key=idempotency_key,
+            octane_version=octane_version,
+            **params
+        )
+
+    @classmethod
+    def create_multi(
+        cls,
+        api_key=None,
+        idempotency_key=None,
+        octane_version=None,
+        measurements=[],
+    ):
+        now = datetime.utcnow().isoformat()
+        for measurement in measurements:
+            if "time" not in measurement:
+                measurement["time"] = now
+        requestor = api_requestor.APIRequestor(
+            api_key, api_version=octane_version
+        )
+        url = cls.class_url() + "multi"
+        headers = util.populate_headers(idempotency_key)
+        response, api_key = requestor.request(
+            "post", url, measurements, headers
+        )
+        return util.convert_to_octane_object(response, api_key, octane_version)
